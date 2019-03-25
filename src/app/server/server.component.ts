c26752c0a37585b096be5d505c908bcc7d0e12a3
nazmul_avanto <nazmul.avanto@gmail.com>
2019-03-25 10:43:55 +0100

    directive with property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,9 +10,15 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = "offline"
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+   }
 
   ngOnInit() {
+  }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 
 }
