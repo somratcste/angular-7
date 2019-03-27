e1919831e7fdd357e084b4950dbea9e6a38750b0
somratcste <somratcste@gmail.com>
2019-03-28 04:17:03 +0600

    create recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,14 +11,12 @@ export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipies: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simply a test', 'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
-    new Recipe('Another Recipe', 'This is a simply a test', 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/IMG_1105.jpg')
-  ];
+  recipies: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipies = this.recipeService.getRecipies();
   }
 
   onRecipeSelected(recipe: Recipe) {
