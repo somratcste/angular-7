e985b4f9b7d98823bf3a7f9ac83673c4c9451711
somratcste <somratcste@gmail.com>
2019-05-26 23:15:17 +0600

    adding shopping list service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,6 +8,7 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipies: Recipe[];
 
@@ -15,5 +16,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
     this.recipies = this.recipeService.getRecipies();
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
